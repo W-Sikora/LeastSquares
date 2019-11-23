9b281f72520ae02a08dc4fe706beb8fa67c2850e
W-Sikora <242612@student.pwr.edu.pl>
2019-11-23 21:40:15 +0100

    new version

--- Method_of_least_squares.py
+++ Method_of_least_squares.py
@@ -25,26 +25,6 @@ def linear_equation(arguments, coefficient_A, coefficient_B):
     return coefficient_A * arguments + coefficient_B
 
 
-def choice(file_name, sheet, number):
-    data = pd.read_excel(file_name, sheet)
-    df = pd.DataFrame(data)
-    length = np.array(df.values).shape[0]
-
-    array = np.zeros(length)
-
-    for i in range(0, length, 1):
-        array[i] = i
-
-    array_choice = np.array(np.random.choice(array, number - 1, replace=False))
-
-    column_0, column_1, column_2 = ([] for i in range(3))
-    for i in range(len(array_choice)):
-        column_0.append(1)
-        column_1.append(df.values[int(array_choice[i])][0])
-        column_2.append(df.values[int(array_choice[i])][1])
-    return np.array([column_0, column_1]), np.array(column_2)
-
-
 def main():
     plt.figure(figsize=(12, 7))
     file_name = "data/residential_premises.xlsx"
@@ -53,13 +33,12 @@ def main():
     data = get_data(file_name, sheet)
     U1 = data[0]
     Y1 = data[1]
-    coefficients_of_linear_equation1 = least_square_fit(U1, Y1)
-    print("Result:\n\tCoefficient A =", coefficients_of_linear_equation1[1],
-          "coefficient B =", coefficients_of_linear_equation1[0])
+    coefficients = least_square_fit(U1, Y1)
+
+    print("Result:\n\tCoefficient A =", coefficients[1], "coefficient B =", coefficients[0])
     plt.plot(U1[1], Y1, 'o', color="royalblue", alpha=0.35, label="data", markersize=4.5)
-    plt.plot(U1[1],
-             linear_equation(U1[1], coefficients_of_linear_equation1[1], coefficients_of_linear_equation1[0]),
-             color="black", alpha=0.8, linewidth=2.2, label="line of best fit")
+    plt.plot(U1[1], linear_equation(U1[1], coefficients[1], coefficients[0]), color="black",
+             alpha=0.8, linewidth=2.2, label="line of best fit")
     plt.xlabel("\nLiving space area [m2]", fontsize=14)
     plt.ylabel("Price [PLN]\n", fontsize=14)
     plt.legend(loc='lower right', fontsize=13)
--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # System Identification
-
 ## Requirements
 - python 3.6
-- matplotlib
 - numpy
-- scipy
 - pandas
+- xlrd
+- scipy
+- matplotlib
 ## To get started
 clone this git repository
 ## Results
 ### Least squares
-Linear regression for residential premises data (least squares method application).
-![result](media/chart.jpg)
 ### Maximum likelihood estimation
 ### Kernel density estimation
+### Data
